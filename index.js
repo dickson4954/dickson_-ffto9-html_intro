f7f15120d32c15b4182fd54a2c98d08488d43036
dickson4954 <dicksonmurithi155@gmail.com>
2024-03-05 11:25:39 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//alert(''dickson'')
+const yob =1999;
+const cy =2024;
+
+const age =cy-yob;
+
+alert(age);
+
+    
---
+++ main.js
@@ -0,0 +1,9 @@
+let price = 50;
+
+const tax = 0.16;
+
+let tot_price = (price * tax) + price;
+
+
+alert(tot_price);
+
